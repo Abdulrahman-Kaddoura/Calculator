ee2133f98fa8eacdd60da4053ea0acad2c3d06db
abd <akaddoura123@gmail.com>
2025-07-01 12:11:15 +0300

    buttons stopped being round. Fixed it

--- RoundedButton.java
+++ RoundedButton.java
@@ -22,14 +22,26 @@ public class RoundedButton extends JButton {
         Graphics2D g2 = (Graphics2D) g.create();
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
+        // Fill rounded background
         if (getModel().isArmed()) {
             g2.setColor(getBackground().darker());
         } else {
             g2.setColor(getBackground());
         }
-
         g2.fillRoundRect(0, 0, getWidth(), getHeight(), radius, radius);
-        super.paintComponent(g2);
+
+        // Draw text manually
+        FontMetrics fm = g2.getFontMetrics();
+        Rectangle textRect = new Rectangle(0, 0, getWidth(), getHeight());
+        int textHeight = fm.getAscent();
+        int textY = (textRect.height - fm.getHeight()) / 2 + textHeight;
+        int textWidth = fm.stringWidth(getText());
+        int textX = (textRect.width - textWidth) / 2;
+
+        g2.setColor(getForeground());
+        g2.setFont(getFont());
+        g2.drawString(getText(), textX, textY);
+
         g2.dispose();
     }
 
@@ -37,7 +49,8 @@ public class RoundedButton extends JButton {
     public void paintBorder(Graphics g) {
         // Graphics2D g2 = (Graphics2D) g.create();
         // g2.setColor(getForeground());
-        // g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        // g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+        // RenderingHints.VALUE_ANTIALIAS_ON);
         // g2.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, radius, radius);
         // g2.dispose();
     }
